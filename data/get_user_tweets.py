0077f4859b845b09a0fef0ee374a812c908cd4fd
Joseph Orduna <joey@Josephs-MacBook-Pro.local>
2020-02-11 12:05:43 -0500

    set up conda environment

--- data/get_user_tweets.py
+++ data/get_user_tweets.py
@@ -1,2 +1,12 @@
 import csv
-import twepy
+import tweepy
+
+if __name__ == "main":
+
+    twitter_cred = dict()
+    twitter_cred["consumer_key"] = PPq7v8RdB2yUaYDH4UgXICfKf 
+    twitter_cred["consumer_secret"] = ZKmr9UjpiaeGBdhITcMbsN7MsQEgWECZUC3fbhQMgPEY1ZNZbF
+    twitter_cred["access_key"] = 2211307145-qbOLwohdFQO2BxA7yjlAG3XrFxtPYltqXdzGWia
+    twitter_cred["access_secret"] = SuXewv5enPTAeaSOJdOx17tu23DXd03tyrv9W9NEVQQiR
+
+ 
