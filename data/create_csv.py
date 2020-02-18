428bd31bba65aab3d62f2df0f264a3d41730e108
Joseph Orduna <joey@Josephs-MBP.fios-router.home>
2020-02-18 02:28:33 -0500

    added nltk functionality

--- data/create_csv.py
+++ data/create_csv.py
@@ -1,28 +1,33 @@
 import os
 import csv
 import json
+from datetime import datetime
 
 if __name__ == "__main__":
-    files = os.listdir(path="Test")
-
+    path = "tweet_complete/"
+    files = os.listdir(path=path)
     data = []
     print(files)
     for file in files:
         try:
-            with open("Test/" + file) as tweet:
+            with open(path + file) as tweet:
                 data.append(json.load(tweet))
         except Exception as e:
             print("Exception Raised-", str(e))
-
-    header = ["usernameTweet", "ID", "text", "url", "nbr_retweet", "nbr_favorite", "nbr_reply", "datetime", "is_reply", "is_retweet", "user_id"]
+    header = ["ID", "usernameTweet",  "datetime", "text", "url", "nbr_retweet", "nbr_favorite", "nbr_reply", "is_reply", "is_retweet", "user_id"]
     csv_rows = []
     for item in data:
         row = []
+        print(item)
         for name in header:
             row.append(item[name])
-        csv_rows.append(row)
+        if item["usernameTweet"] == "cambrasine":
+            csv_rows.append(row)
 
-    with open("cambrasine_tweets.csv", "w") as csv_out:
+    csv_rows.sort(key=lambda date: datetime.strptime(date[2], "%Y-%m-%d %H:%M:%S"))
+
+    print(csv_rows)
+    with open("cambrasine_only_tweets.csv", "w") as csv_out:
         writer = csv.writer(csv_out, delimiter=",")
         writer.writerow(header)
         writer.writerows(csv_rows)
